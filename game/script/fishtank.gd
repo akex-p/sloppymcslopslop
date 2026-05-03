2ba470d7e640fce9fe73e4b41fa7b8e5217c01e4
Johannes <jhannesmueller0@gmail.com>
2026-05-03 23:57:48 +0200

    piranha can be spawned from code

--- game/script/fishtank.gd
+++ game/script/fishtank.gd
@@ -1,5 +1,11 @@
 extends Interactable
 
+@export var piranha_scene : PackedScene
+@export var piranha_spawnpoint : Node3D
+@export var piranha_food: Array[Node3D] = []
+
+var piranha
+
 func _ready():
 	super()
 	prompt_text = "Feed Fish"
@@ -7,3 +13,9 @@ func _ready():
 
 func interact() -> void:
 	GameManager.advance_step()
+
+func _release_the_piranha() -> void:
+	piranha = piranha_scene.instantiate()
+	piranha.targets = piranha_food
+	get_tree().current_scene.add_child(piranha)
+	piranha.global_position = piranha_spawnpoint.global_position
