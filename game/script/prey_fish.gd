91408c95114f871ebb9f5bb18fded1172c0d5ff8
akex <143971929+akex-p@users.noreply.github.com>
2026-05-04 13:34:38 +0200

    change texture... ?

--- game/script/prey_fish.gd
+++ game/script/prey_fish.gd
@@ -1,16 +1,11 @@
 extends Node3D
 
 @export var blood_scene := preload("res://scene/fishes/fish_gone.tscn")
+@export var new_texture: CompressedTexture2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	if new_texture != null:
+		$Plane.get_surface_override_material(0).set_shader_parameter("texture_albedo", new_texture)
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if(area.is_in_group("predator_fish")):
