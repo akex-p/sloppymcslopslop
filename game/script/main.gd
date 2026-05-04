b09be1233d05819e5b274d60064681a27b93f986
akex <143971929+akex-p@users.noreply.github.com>
2026-05-04 13:23:04 +0200

    newest build and fullscreen

--- game/script/main.gd
+++ game/script/main.gd
@@ -32,6 +32,13 @@ func _unhandled_input(event):
 	if event.is_action_pressed("reload_game"):
 		GameManager.reset()
 		get_tree().reload_current_scene()
+	
+	if event.is_action_pressed("fullscreen"):
+		var mode = DisplayServer.window_get_mode()
+		if mode == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 func wake_up():
 	if not ad_played: return
